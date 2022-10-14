8f7b8af0bc785a220d27fb393a80955466c2070a
JuanParraIV <jmparra1993@gmail.com>
2022-10-14 11:09:58 -0500

    segundo commit

--- mercurycash/components/Register/index.tsx
+++ mercurycash/components/Register/index.tsx
@@ -1,31 +1,153 @@
-import Router from 'next/router'
-import React from 'react'
+import Router, { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import EyeIcon from '../Icons/eyeIcon'
+import LockIcon from '../Icons/lockIcon'
+import SearchIcon from '../Icons/searchIcon'
 import CheckInput from '../Shared/Form/inputCheck'
-import PasswordInput from '../Shared/Form/passwordInput'
-import SelectCountryInput from '../Shared/Form/selectCountryBox'
-import SelectLanguageInput from '../Shared/Form/selectLanguageBox'
 import SubmitButton from '../Shared/Form/submitButton'
-import TextInput from '../Shared/Form/textInput'
 import Logo from '../Shared/Logo'
+import api from '../../pages/api/create'
+import { v4 as uuid } from 'uuid';
+
+interface FormData {
+  email: string;
+  password: string;
+  country: string;
+  lang: string;
+  retype?: string;
+  id?: string;
+}
+
 
 const RegisterForm = () => {
+  const router = useRouter();
+  const [users, setUsers] = useState([]);
+  const [form, setForm] = useState<FormData>({
+    email: "",
+    password: "",
+    country: "",
+    lang: ""
+  });
+
+  const retrieveContacts = async () => {
+    const response = await api.get('/users');
+    return response.data
+  }
+  const addUsers = async (data: FormData) => {
+    const request = {
+      id: uuid(),
+      ...data
+    }
+    const response = await api.post("/users", request)
+  }
+
+  const refreshPage = () => {
+    router.replace(router.asPath);
+  };
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target as HTMLInputElement | HTMLSelectElement;
+
+    setForm(() => ({
+      ...form,
+      [name]: value,
+    }));
+
+  }
+
+  const handleSubmit = async (data: FormData) => {
+    try {
+      addUsers(data);
+      setForm({
+        email: "",
+        password: "",
+        country: "",
+        lang: ""
+      });
+      refreshPage();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const allUsers = await retrieveContacts();
+      if (allUsers) setUsers(allUsers);
+    }
+    getAllUsers();
+  }, [])
+  console.log(form)
+  console.log(users)
+
+
   return (
     <div className='bg-bg_primary w-50'>
-      <form className='flex flex-col w-full items-center justify-center gap-5 py-12'>
+      <form className='flex flex-col w-full items-center justify-center gap-5 py-12' onSubmit={(event) => {
+        event.preventDefault();
+        handleSubmit(form);
+      }} >
         <Logo />
         <h1 className='text-center text-2xl text-primary'>Create your Account</h1>
         <div className='flex relative flex-col  items-center justify-center gap-5'>
-          <TextInput
-            placeholder='Email'
-          />
-          <PasswordInput
-            placeholder='Password'
-          />
-          <PasswordInput
-            placeholder='Retype Password'
-          />
-          <SelectCountryInput />
-          <SelectLanguageInput />
+          <div className='flex justify-end items-center relative'>
+            <input
+              type="text"
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder='Email'
+              className='rounded-lg text-gray-200 p-3 border border-gray-200 placeholder:text-gray-200' />
+            <LockIcon />
+          </div>
+
+          <div className='flex justify-end items-center relative'>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              name='password'
+              value={form.password}
+              onChange={handleChange}
+              className='rounded-lg text-gray-200 p-3 border border-gray-300 placeholder:text-gray-200'
+            />
+            <EyeIcon />
+          </div>
+
+          <div className='flex justify-end items-center relative'>
+            <input
+              type="password"
+              required
+              placeholder='Retype Password'
+              className='rounded-lg text-gray-200 p-3 border border-gray-300 placeholder:text-gray-200'
+            />
+            <EyeIcon />
+          </div>
+
+          <div className='flex w-[280px] justify-start items-center relative'>
+            <SearchIcon />
+            <select
+              name='country'
+              onChange={handleChange}
+              className='rounded-3xl w-full text-primary bg-white p-3 border border-gray-200 px-10'>
+              <option value="" selected>Country of Residence</option>
+              <option value="Colombia">Colombia</option>
+              <option value="USA">USA</option>
+              <option value="CANADA">CANADA</option>
+            </select>
+          </div>
+
+          <div className='flex w-[280px] justify-start items-center relative'>
+            <select
+              name='lang'
+              onChange={handleChange}
+              className='rounded-3xl w-full text-primary bg-white p-3 border border-gray-200 px-10'>
+              <option value="" selected>Select your language</option>
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+              <option value="French">French</option>
+            </select>
+          </div>
 
 
           <div className='p-2 w-[250px]'>
@@ -42,5 +164,4 @@ const RegisterForm = () => {
     </div>
   )
 }
-
 export default RegisterForm
--- mercurycash/components/Shared/Form/textInput.tsx
+++ mercurycash/components/Shared/Form/textInput.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LockIcon from '../../Icons/lockIcon'
 
 interface Props {
+
   placeholder?: string
 }
 
